d34ab1db0dada87234156254dcf870943782651c
PIE <35545930+RealPIEGamer@users.noreply.github.com>
2024-12-17 12:20:53 -0500

    Update and rename ish_client.lua to sish_client.lua

--- sish_client.lua
+++ sish_client.lua
@@ -4,26 +4,23 @@
 write("modem side = ")
 modem_side = peripheral.find("modem") -- modified so it doesnt as so many damn questions, just input hostname and RUNNNNN
 write("Hostname = ")
-hostname = io.read()
+hostname = "insert host here" -- this way if you run this as startup you can just plug and play
 protocol = "SISH"
 
 -- Check modem
 print("Checking modem...")
 local modem = peripheral.wrap(modem_side)
 if modem.isWireless() then
-  print("Selected modem is wireless.")
+  print("Connected")
 else
-  print("Selected modem is wired and will not work with turtles.")
+  print("Connected - Wired Only")
 end
 
 -- Check if rednet is open
-print("Checking if rednet is open... " .. tostring(rednet.isOpen(modem_side)))
+print("Starting SISH v1.0 - Opening RedNet" .. tostring(rednet.isOpen(modem_side)))
 if not rednet.isOpen(modem_side) then
   -- Open rednet
-  print("Opening rednet...")
   rednet.open(modem_side)
-else
-  print("Rednet is already open...")
 end
 
 -- Register hostname
@@ -33,12 +30,12 @@ rednet.host(protocol, hostname)
 -- ~Client Specific code~
 
 -- Get the ID of the slave
-write("Slave = ")
+write("Enter device ID for connection: ")
 slave = io.read()
 slaveID = rednet.lookup(protocol, slave)
 
 while true do
-  write("<ish@" .. slave .. "> ")
+  write("<sish@" .. slave .. "> ")
   remoteCmd = io.read()
   if remoteCmd == "exit" then
     break
@@ -52,7 +49,7 @@ while true do
   if (senderID == slaveID and recvMsg) then
     print("Success.")
   else
-    print("ERROR: Slave did not return success.")
+    print("ERROR: Could not Connect.")
   end
 end
 
